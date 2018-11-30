e9f3f4476a950e9ae18c96055ee5c6c9bb7d98a5
r0717250 <r0712750@thomasmore.student.be>
2018-11-30 12:16:29 +0100

    getters setters

--- src/main/java/be/thomasmore/travelmore/domain/Gebruiker.java
+++ src/main/java/be/thomasmore/travelmore/domain/Gebruiker.java
@@ -24,6 +24,15 @@ public class Gebruiker {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int id;
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
     @Column(name = "voornaam")
     private  String voornaam;
     @Column(name = "achternaam")
@@ -38,8 +47,65 @@ public class Gebruiker {
     private String wachtwoord;
 
 
+
+
     @ManyToOne(cascade = CascadeType.MERGE)
     @JoinColumn(name = "soortGebruikerID")
     private SoortGebruiker soortGebruiker;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public String getGemeente() {
+        return gemeente;
+    }
+
+    public void setGemeente(String gemeente) {
+        this.gemeente = gemeente;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWachtwoord() {
+        return wachtwoord;
+    }
+
+    public void setWachtwoord(String wachtwoord) {
+        this.wachtwoord = wachtwoord;
+    }
+
+    public SoortGebruiker getSoortGebruiker() {
+        return soortGebruiker;
+    }
+
+    public void setSoortGebruiker(SoortGebruiker soortGebruiker) {
+        this.soortGebruiker = soortGebruiker;
+    }
 }
