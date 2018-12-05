690212479575551e5d24042c5c2cd00f4defa35e
jens.vanhoof <jens.vanhoof@imalle.be>
2018-12-05 17:17:06 +0100

    bookingen update

--- src/main/java/be/thomasmore/travelmore/controller/BookingController.java
+++ src/main/java/be/thomasmore/travelmore/controller/BookingController.java
@@ -8,6 +8,7 @@ import be.thomasmore.travelmore.service.ReisService;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
+import java.awt.print.Book;
 import java.util.List;
 
 @ManagedBean(name = "BookingController")
@@ -34,8 +35,8 @@ public class BookingController {
         return this.bookingService.findBookingById(geselecteerdeBooking.getId());
     }
 
-    public List<Reis> getReizenByUserId(int gebruikersID){
-        return this.bookingService.findAllReizenByUserBookings(gebruikersID);
+    public List<Booking> getReizenByUserId(int gebruikersID){
+        return this.bookingService.findAllBookingsByUserId(gebruikersID);
     }
 
     public String navigateToBooken(int id){
