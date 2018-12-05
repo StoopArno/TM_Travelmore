8bf2ca2db6ad8c6afe5c8d21496522af56f3c83e
FlorianDHaene <dhaene_florian@hotmail.com>
2018-12-05 13:57:57 +0100

    opmaak index

--- src/main/java/be/thomasmore/travelmore/domain/Reis.java
+++ src/main/java/be/thomasmore/travelmore/domain/Reis.java
@@ -29,11 +29,6 @@ import java.util.GregorianCalendar;
                         query = "SELECT r.aankomstLocatie.id FROM Reis r"
                 ),
                 @NamedQuery(
-<<<<<<< HEAD
-                      name = Reis.FILTERVERTREKLOCATIE,
-                       query = "SELECT r FROM Reis r Where lower(r.aankomstLocatie.naam) LIKE :naam"
-                )
-=======
                        name = Reis.FILTERVERTREKLOCATIE,
                         query = "SELECT r FROM Reis r Where lower(r.vertrekLocatie.naam) = :naam"
                 ),
@@ -53,15 +48,10 @@ import java.util.GregorianCalendar;
                         name = Reis.FILTERTRANSPORTMIDDEL,
                         query = "SELECT r FROM Reis r Where lower(r.transportmiddel) = :naam"
                 ),
-<<<<<<< HEAD
                 @NamedQuery(
                         name = Reis.FILTERVERTREKTIJD,
                         query = "SELECT r FROM Reis r Where r.vertrekTijd like  :vertrektijd"
                 )
-
-=======
->>>>>>> 590f37565f7515e77b119d9c3b07089b9640fa0d
->>>>>>> 8871762288daa921de4a5a9d1109c918da6dbe6f
         }
 )
 public class Reis {
